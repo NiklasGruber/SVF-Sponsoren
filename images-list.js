56d738b64236bdd1528351c898d4a47ee170ff10
Niklas Gruber <niklas.gruber.02@gmail.com>
2026-03-21 12:49:40 +0100

    more pics

--- images-list.js
+++ images-list.js
@@ -4,5 +4,7 @@ const imagesList = [
   "images/1278650591628556536_a94e141603750fbb4ea3-1,0-320x320.png",
   "images/641641852_1508180047978338_1043191311908302583_n.jpg",
   "images/650085333_1521299929999683_395899873244240769_n.jpg",
-  "images/HeaderSlider_PVB_2025-07-25-061136_ggqa.png"
+  "images/HeaderSlider_PVB_2025-07-25-061136_ggqa.png",
+  "images/MaibaumKraxxlnPlackat.jpeg",
+  "images/wb_logo_kringel.png"
 ];
